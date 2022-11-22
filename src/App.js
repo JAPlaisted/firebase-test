3c1b4c7d1c8c4afa3589bb3306cb86bde33450e7
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-11-21 18:23:43 -0800

    firetest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5AxHSdQqK_YP8avbyIhmNQ22cfS9ygKo",
+  authDomain: "firetest-577f2.firebaseapp.com",
+  projectId: "firetest-577f2",
+  storageBucket: "firetest-577f2.appspot.com",
+  messagingSenderId: "296828644208",
+  appId: "1:296828644208:web:85933e15413b01889a7db5",
+  measurementId: "G-B7E8XY0W0M",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
